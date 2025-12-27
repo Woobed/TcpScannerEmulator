686721188ad4de55f6be4c0b299ff4c6a2480460
n.shindyapin <n.shindyapin@trace-x.ru>
2025-12-27 16:18:40 +0300

    reactive property addition

---
+++ ScannerEmulator2.0/ReactiveProperty/ReactiveProperty.cs
@@ -0,0 +1,20 @@
+﻿namespace ScannerEmulator2._0.ReactiveProperty
+{
+    public class ReactiveProperty<T>
+    {
+        public Action<T?>? OnProperyChanged { get; set; }
+        private T? _value { get; set; }
+        public T? Value
+        {
+            get
+            {
+                return _value;
+            }
+            set
+            {
+                _value = value;
+                OnProperyChanged?.Invoke(_value);
+            }
+        }
+    }
+}
